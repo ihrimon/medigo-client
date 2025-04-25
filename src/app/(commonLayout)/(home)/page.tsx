7c7af0cd59cd43b15288bb0927ab56080e7fe51f
ihrimon <ihrimon147@gmail.com>
2025-04-25 20:13:35 +0600

    added

--- src/app/(commonLayout)/(home)/page.tsx
+++ src/app/(commonLayout)/(home)/page.tsx
@@ -1,14 +1,14 @@
 import HomeBanner from '@/components/modules/home/HomeBanner';
-import HomeGallery from '@/components/modules/home/HomeGallery';
+// import HomeGallery from '@/components/modules/home/HomeGallery';
 // import { HotSale } from '@/components/modules/home/HotSale';
-import LatestBlog from '@/components/modules/home/LatestBlog';
-import PopularBrands from '@/components/modules/home/PopularBrands';
-import PopularCategories from '@/components/modules/home/PopularCategories';
+// import LatestBlog from '@/components/modules/home/LatestBlog';
+// import PopularBrands from '@/components/modules/home/PopularBrands';
+// import PopularCategories from '@/components/modules/home/PopularCategories';
 import ServiceMarquee from '@/components/modules/home/ServiceMarquee';
-import Testimonials from '@/components/modules/home/Testimonials';
+// import Testimonials from '@/components/modules/home/Testimonials';
 import TopCategories from '@/components/modules/home/TopCategories';
 import { TrendingItems } from '@/components/modules/home/TrendingItems';
-import WorkProcess from '@/components/modules/home/WorkProcess';
+// import WorkProcess from '@/components/modules/home/WorkProcess';
 
 export default function Home() {
   return (
@@ -17,13 +17,12 @@ export default function Home() {
       <ServiceMarquee />
       <TopCategories />
       <TrendingItems />
-      {/* <HotSale /> */}
-      <PopularCategories />
+      {/* <PopularCategories />
       <PopularBrands />
       <WorkProcess />
       <Testimonials />
       <LatestBlog />
-      <HomeGallery />
+      <HomeGallery /> */}
     </div>
   );
 }
